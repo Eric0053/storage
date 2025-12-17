12ef69be903e19b4466da9a4153882bca1390e03
Eric <az710053@gmial>
2025-12-18 01:06:56 +0800

    revert to 53ab668f

--- gitSample/gitSample/Program.cs
+++ gitSample/gitSample/Program.cs
@@ -5,17 +5,6 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("test");
-            Console.WriteLine("test2");
-            Console.ReadLine();
-            List<string> list = new List<string> {"a","b","c" };
-            
-            for (int i = 0; i < list.Count; i++) 
-            {
-                Console.WriteLine(list[i]);
-                Console.ReadLine();
-
-            }
         }
     }
 }
